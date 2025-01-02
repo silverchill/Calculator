49cab28121dd4a044460f7c01ee3e7a4328d2974
silverchill <ustinsteve@gmail.com>
2025-01-02 14:36:53 +0100

    Refactor code to use grid layout; implemented % functionality, date/time handling, and icon placement

--- calculator.js
+++ calculator.js
@@ -16,13 +16,23 @@ const deleteLast = (displayScreen) => {
     calculateResult();
 };
 
+// Function to calculate percentage
+const percentage = (displayScreen, displayScreen2) => {
+    let currentValue = parseFloat(displayScreen.textContent);
+    if (!isNaN(currentValue)) {
+        displayScreen.textContent += "%";
+        displayScreen2.textContent = (currentValue / 100);
+    }
+};
+
 // Function to calculate the result of the expression
 const calculateResult = () => {
     const displayScreen = document.getElementById("display1");
     const displayScreen2 = document.getElementById("display2");
     try {
         // Evaluate the mathematical expression in the displayScreen
-        const result = eval(displayScreen.textContent);
+        const result = eval(displayScreen.textContent.replace("%", "/100"));
+
         displayScreen2.textContent = result;
     } catch (error) {
         displayScreen2.textContent = ""; // Show error if the expression is invalid
@@ -41,14 +51,34 @@ document.addEventListener("DOMContentLoaded", () => {
             const value = span ? span.textContent.trim() : "";
 
             if (value === "cls") {
-                clearDisplayScreen(displayScreen, displayScreen2); // Clear displayScreen for "cls"
+                clearDisplayScreen(displayScreen, displayScreen2);
             } else if (value === "DEL") {
-                deleteLast(displayScreen, displayScreen2); // Delete last character for "DEL"
+                deleteLast(displayScreen, displayScreen2);
+            } else if (value === "%") {
+                percentage(displayScreen, displayScreen2);
             } else if (value === "=") {
-                calculateResult(displayScreen, displayScreen2); // Calculate the result when "=" is clicked
+                calculateResult(displayScreen, displayScreen2);
             } else {
                 appendToDisplayScreen(displayScreen, value); // Add the clicked value to the displayScreen
             }
         });
     });
 });
+
+// To display current time and day
+const displayTime = document.getElementById('run-time');
+
+const updateDay = () => {
+    const currentDay = new Date();
+    const day = currentDay.toLocaleDateString();
+    document.getElementById('today').innerText = day;
+};
+updateDay();
+
+const updateTime = () => {
+    const currentDay = new Date();
+    const time = currentDay.toLocaleTimeString();
+    displayTime.textContent = time;
+}
+setInterval(updateTime, 1000);
+
